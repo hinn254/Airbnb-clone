5c46d007eaea366e08745fc60a7a9592b5ab1727
Benny <bennyhinnotieno@gmail.com>
2020-10-18 17:11:44 +0300

    finalised build

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>&copy; 2020 Airtbnb clone! No rights reserced - this is a demo</p>
+      <p>Privact - Terms - Sitemap - Company Details</p>
+    </div>
+  );
+};
+
+export default Footer;
