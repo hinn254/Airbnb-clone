9363d90ff61fa9233e65b9ecffb14741649e49cd
Benny <bennyhinnotieno@gmail.com>
2020-10-19 15:05:25 +0300

    modified footer

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,7 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <div className="footer">
-      <p>&copy; 2020 Airtbnb clone! No rights reserced - this is a demo</p>
+      <p>&copy; 2020 Airtbnb clone! - BennyWorld</p>
       <p>Privact - Terms - Sitemap - Company Details</p>
     </div>
   );
